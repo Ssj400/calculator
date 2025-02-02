04cfae3ca76320eceb4782f8131cb32051d6aa38
ssj400 <gafetel2017@gmail.com>
2025-02-02 11:10:48 -0300

    Added principal calculator functionality
    Sum, subtract, multiply, divide and delete.

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-let buttons = document.querySelectorAll(".button.displays")
-let output = document.querySelector(".output p")
+let buttons = document.querySelectorAll(".button.displays");
+let output = document.querySelector(".output p");
+let equal = document.querySelector("#equal");
+let ac = document.querySelector(".button.deleteAll");
+let eliminate = document.querySelector(".button.delete");
+let operators = "+-x÷"
+let operation;
 
 let add = (a, b) => {
     return a + b;
@@ -24,17 +29,39 @@ let operate = (a, operator, b) => {
         return subtract(a,b);
     } else if (operator == 'x') {
         return multiply(a,b);
-    } else if (operator == '/') {
+    } else if (operator == '÷') {
         return divide(a, b);
     }
 }
+
+let detectOperation = (str) => {
+    let arr = str.split("");
+    let operator = arr.find((item) => operators.includes(item));
+    let firstNumber = arr.slice(0, arr.findIndex((item) => item == operator));
+    let secondNumber = arr.slice(arr.findIndex((item) => item == operator) + 1);
+    output.textContent = operate(Number(firstNumber.join('')), operator, Number(secondNumber.join('')));
+}   
 
 
 buttons.forEach((item) => {
     item.addEventListener('click', () => {
         if (output.textContent.length < 12) {
             output.textContent += item.textContent;
+            operation = output.textContent;
         } 
     })
 })
 
+equal.addEventListener('click', () => {
+    detectOperation(operation);
+})
+
+ac.addEventListener('click', () => {
+    output.textContent = '';
+    operation = '';
+})
+
+eliminate.addEventListener('click', () => {
+    output.textContent = output.textContent.substring(0, output.textContent.length -1);
+    operation = output.textContent;
+})

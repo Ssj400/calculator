cc30fe0abe56ef3dcbbd9fb1219b061e5f853b3d
ssj400 <gafetel2017@gmail.com>
2025-02-03 16:19:01 -0300

    Added power operator, and +/-
    Also eliminated () and corrected calculating
    errors.

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let buttons = document.querySelectorAll(".button.displays");
+let buttonsOperator = document.querySelectorAll(".operation");
 let output = document.querySelector(".output p");
 let equal = document.querySelector("#equal");
 let ac = document.querySelector(".button.deleteAll");
 let eliminate = document.querySelector(".button.delete");
-let operators = "+-x÷"
-let operation;
-
-let hasDuplicate = (arr) => arr.some((val, i) => arr.indexOf(val) !== i)
+let negative = document.querySelector(".negative");
+let operators = "+-x÷^"
+let operation = '';
+let operatorClicked = false;
+let operationNegative;
+let operationNegative2;
 
 let add = (a, b) => {
     return a + b;
@@ -24,6 +27,10 @@ let divide = (a, b) => {
     return (a / b).toFixed(4);
 }
 
+let power = (a, b) => {
+    return a ** b;
+}
+
 let operate = (a, operator, b) => {
     if (operator == '+') {
         return add(a, b);
@@ -33,48 +40,46 @@ let operate = (a, operator, b) => {
         return multiply(a, b);
     } else if (operator == '÷') {
         return divide(a, b);
+    } else if (operator == '^') {
+        return power(a, b);
     }
 }
 
 let detectOperation = (str) => {
     let arr = str.split("");
-    let arrOperatorIndex = [];
-    let arrOperator = arr.filter((item, index) => {
-        if (operators.includes(item)) {
-            arrOperatorIndex.push(index)
-            return true;
-        }
-    });
     let operator = arr.find((item) => operators.includes(item));
-    let firstNumber = arr.slice(0, arr.findIndex((item) => item == operator))
-                        .join('');
-    let secondNumber;
+    let firstNumber;
 
-    if (arrOperator.length > 1) {
-        let result;
-        for (let i = 0; i < arrOperatorIndex.length; i++) {
-
-            if (i !== 0) {
-                firstNumber = result;
-            }
-
-            operator = arr[arrOperatorIndex[i]];
-
-            if (arrOperatorIndex.length > i + 1) {
-                secondNumber = arr.slice(arrOperatorIndex[i] + 1, arrOperatorIndex[i + 1]);
-            } else {
-                secondNumber = arr.slice(arrOperatorIndex[i] + 1);
-            }
-
-            result = operate(Number(firstNumber), operator, Number(secondNumber.join('')));
-        }
-        output.textContent = result;
-        return;
+    if (arr.indexOf(operator) == 0) {
+        arr.shift();
+        operator = arr.find((item) => operators.includes(item));
+        firstNumber = arr.slice(0, arr.findIndex((item) => item == operator))
+            .join('');
+        firstNumber *= -1;
+    } else {
+        firstNumber = arr.slice(0, arr.findIndex((item) => item == operator))
+            .join('');
     }
 
 
-    secondNumber = arr.slice(arr.findIndex((item) => item == operator) + 1);
-    output.textContent = operate(Number(firstNumber), operator, Number(secondNumber.join('')));
+
+    secondNumber = arr.slice(arr.findIndex((item) => item == operator) + 1)
+        .join("");
+
+
+    if (operate(Number(firstNumber), operator, Number(secondNumber)) !== undefined) {
+
+        let result = operate(Number(firstNumber), operator, Number(secondNumber));
+
+        if (Number.isNaN(result) || Number.isNaN(output.textContent)) {
+            console.log(result)
+            return output.textContent = 'Error';
+        } else if (result > 10 ** 12) {
+            return output.textContent = 'Too big';
+        }
+        return output.textContent = result;
+    }
+
 }
 
 
@@ -87,16 +92,70 @@ buttons.forEach((item) => {
     })
 })
 
+buttonsOperator.forEach((item) => {
+    item.addEventListener('click', () => {
+
+        if (operators.includes(operation)) {
+            operation = '';
+            output.textContent = '';
+            return;
+        }
+
+        if (operatorClicked == true) {
+            let newOperation = operation.split("")
+            operation = newOperation.pop();
+            output.textContent = `${detectOperation(newOperation.join(''))}${operation}`;
+            operation = output.textContent;
+        }
+        operatorClicked = true;
+    })
+})
+
 equal.addEventListener('click', () => {
     detectOperation(operation);
+    operatorClicked = false;
 })
 
 ac.addEventListener('click', () => {
     output.textContent = '';
     operation = '';
+    operatorClicked = false;
 })
 
 eliminate.addEventListener('click', () => {
+    if (operators.includes(output.textContent.substring(output.textContent.length - 2, output.textContent.length - 1))) {
+        operatorClicked = false;
+    }
     output.textContent = output.textContent.substring(0, output.textContent.length - 1);
     operation = output.textContent;
+})
+
+negative.addEventListener('click', () => {
+    let arrOperation = String(operation).split('');
+    let operator = arrOperation.find((item) => operators.includes(item));
+    let arrOfOperators = arrOperation.filter((item) => operators.includes(item))
+
+    if (operator == undefined || arrOperation.findIndex((item) => item == operator) == 0 && arrOfOperators.length == 1) {
+
+        operation *= -1;
+        output.textContent = operation;
+
+    } else if (operator == arrOperation[0]){
+
+        arrOperation.shift();
+        operator = arrOperation.find((item) => operators.includes(item));
+        operationNegative2 = arrOperation.splice(arrOperation.indexOf(operator) + 1);
+        arrOperation.push(operationNegative2.join('') * -1);
+        arrOperation.unshift("-");
+        operation = arrOperation.join('');
+        output.textContent = operation;
+
+    } else {
+
+        operationNegative = arrOperation.splice(arrOperation.indexOf(operator) + 1)
+        arrOperation.push(operationNegative.join('') * -1);
+        operation = arrOperation.join('');
+        output.textContent = operation;
+
+    }
 })
